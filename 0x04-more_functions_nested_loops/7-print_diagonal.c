df4182d6783343e2df03cefe92bb78fa7f03efdb
Kamal Abdelmalek <malikleet1337@gmail.com>
2023-10-13 18:27:52 -0700

    Function that print in diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry point
+ * @n : Is the number of time the character \ should be printed
+ * Prototype: void print_diagonal(int n)
+ * Return: If n is 0 or less, the function should only print a \n
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+
+}
