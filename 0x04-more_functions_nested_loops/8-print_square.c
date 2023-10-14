201a65b3e92840e4cf1847c06c001e9f0fea2da7
Kamal Abdelmalek <malikleet1337@gmail.com>
2023-10-14 05:29:19 -0700

    funcition that prints squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_square - Entry point
+ *  
+ * @size: is the size of the sauqre
+ *
+ * Portotype: void print_square(int size)
+ * 
+ * Description: Write a function that prints a square.
+ *
+ * Return: 0 (Success)
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for(j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
