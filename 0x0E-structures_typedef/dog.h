d514f9109d77b90bdb2b47bbe296359a264aaa58
Kamal Abdelmalek <malikleet1337@gmail.com>
2023-11-06 15:23:38 -0800

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdlib.h>
 
-struct dog {
+/**
+ * struct dog - dog's struct
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+
+typedef struct dog {
     char *name;
     float age;
     char *owner;
-};
+}dog_t;
 
 #endif
 
