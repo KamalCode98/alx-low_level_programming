937269a95a6f0282f3fb0500fd6f8c1a77870a02
Kamal Abdelmalek <malikleet1337@gmail.com>
2023-11-20 15:45:39 -0800

    correction 2

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,13 @@
 #include "lists.h"
 
 /**
- * free_listint = Frees a listint_t list
+ * free_listint - Frees a listint_t list
+ *
  * @head: A pointer to the head of the listint_t list
+ *
+ * Description: Frees listint_t list
+ *
+ * Return: Void
  */
 
 void free_listint(listint_t *head)
