2c984b0eced935568edc24479ea78fdb08dc21f3
Kamal Abdelmalek <malikleet1337@gmail.com>
2023-11-20 13:14:45 -0800

    correction num6

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,6 +5,10 @@
  *
  *  * @h: linked list head
  *
+ * Description: Function that prints all the elements of a listint_t list.
+ *
+ * Prototype: size_t print_listint(const listint_t *h);
+ *
  * Return: the number of nodes
  *
  */
@@ -12,14 +16,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int cnt = 0;
+	int i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		cnt++;
+		i++;
 	}
 
-	return (cnt);
+	return (i);
 }
