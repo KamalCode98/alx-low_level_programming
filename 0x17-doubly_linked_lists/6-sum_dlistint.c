c528da28c29a9000ab84eb973dcfb1f8fb448b85
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-01-12 04:50:02 -0800

    commit7

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Calculate the sum of all the data (n) in a dlistint_t linked list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: Sum of all data in the list, or 0 if the list is empty.
+ */
