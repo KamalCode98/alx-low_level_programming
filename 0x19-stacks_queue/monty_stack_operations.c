9912fdda03cb0122a6aa2cb591fb89227f551a50
Kamal <malikleet1337@gmail.com>
2024-01-17 17:12:28 +0100

    adding the function print stack elements

--- 0x19-stacks_queue/monty_stack_operations.c
+++ 0x19-stacks_queue/monty_stack_operations.c
@@ -24,3 +24,24 @@ void push_to_stack(stack_t **new_node, __attribute__((unused))unsigned int line_
     tmp->prev = head;
 }
 
+/**
+ * print_stack_elements - Prints the elements of the stack.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: Line number of the opcode (unused in this function).
+ */
+void print_stack_elements(stack_t **stack, __attribute__((unused))unsigned int line_number)
+{
+    stack_t *tmp;
+
+    (void)line_number;
+
+    if (stack == NULL)
+        exit(EXIT_FAILURE);
+
+    tmp = *stack;
+    while (tmp != NULL)
+    {
+        printf("%d\n", tmp->n);
+        tmp = tmp->next;
+    }
+}
