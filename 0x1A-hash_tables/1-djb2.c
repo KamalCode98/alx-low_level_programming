70bdcb6f02b4f65adeaf2e96df87603fd950ff71
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-02-02 14:43:39 -0800

    betty 2djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,18 +5,17 @@
  * @str: The string to hash.
  *
  * Return: Calculated HASH.
- */ 
+ */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	
+
 	int c;
 
 	hash = 5381;
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	
 
 	return (hash);
 }
